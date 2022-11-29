59523f4a4197139303193b507b4b2b616d016b53
dhain <d.hain@gmx.at>
2022-11-29 10:53:23 +0100

    added Position model

---
+++ frontend/models/Position.ts
@@ -0,0 +1,20 @@
+class Position {
+    private _x: number;
+    private _y: number;
+
+    get x(): number {
+        return this._x;
+    }
+
+    set x(value: number) {
+        this._x = value;
+    }
+
+    get y(): number {
+        return this._y;
+    }
+
+    set y(value: number) {
+        this._y = value;
+    }
+}
