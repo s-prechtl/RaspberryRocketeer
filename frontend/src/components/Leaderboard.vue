6a94e967e1d2952dbc05164dcb545fa7d184be6f
s-prechtl <stefan@tague.at>
2023-01-24 10:38:12 +0100

    leaderboard better

--- frontend/src/components/Leaderboard.vue
+++ frontend/src/components/Leaderboard.vue
@@ -9,8 +9,11 @@
         <RRButton @click="nextPage" text=">"></RRButton>
       </div>
     </div>
-    <div class="row" v-for="entry in this.page" :key="entry.rank">
+    <div class="row" v-if="this.page.length > 0" v-for="entry in this.page" :key="entry.rank">
       <LeaderboardEntry :entry="entry"></LeaderboardEntry>
+    </div>
+    <div class="row" v-else>
+      Loading...
     </div>
   </div>
 </template>
@@ -39,11 +42,7 @@ export default {
   },
   created() {
     this.updatePage();
-    window.addEventListener('itemInserted', (event) => {
-      if (event.key === 'game-isRunning' && event.value === 'false'){
-        this.updatePage();
-      }
-    }, false)
+    window.addEventListener('itemInserted', event => this.onItemInserted(event), false);
   },
   methods: {
     async fetchPage() {
@@ -54,20 +53,29 @@ export default {
       return this.type === "totalplaytime" ? "Total Playtime" : "Highscore";
     },
     nextPage() {
+      if (this.page.length !== this.entriesPerPage) return;
+
       this.pageNumber++;
       this.updatePage();
     },
     prevPage() {
-      if (this.pageNumber > 0) this.pageNumber--;
+      if (this.pageNumber <= 0) return;
+
+      this.pageNumber--;
       this.updatePage();
     },
     async updatePage() {
       let tempPage = await this.fetchPage();
       for (let i = 0; i < this.entriesPerPage; i++) {
-        this.page.pop()
+        this.page.pop();
       }
       for (const entry of tempPage) {
-        this.page.push(entry)
+        this.page.push(entry);
+      }
+    },
+    onItemInserted(event) {
+      if (event.key === 'game-isRunning' && event.value === 'false') {
+        this.updatePage();
       }
     }
   }
--- frontend/src/components/LeaderboardEntry.vue
+++ frontend/src/components/LeaderboardEntry.vue
@@ -1,17 +1,18 @@
 <template>
-  <div class="col-1 text-left">{{this.entry.rank}}</div>
-  <div class="col text-left">{{this.entry.username}}</div>
-  <div class="col text-right">{{this.entry.score}}</div>
+  <div class="col-1 text-right">{{this.entry.rank}}</div>
+  <span class="col offset-1 text-left username">{{this.entry.username}}</span>
+  <div class="col-2 text-right">{{this.entry.score}}</div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {defineComponent, PropType} from 'vue';
+import {Leaderboard, LeaderboardEntry} from "@/model/Leaderboard";
 
 export default defineComponent({
   name: "LeaderboardEntry",
   props: {
     entry: {
-      type: Object,
+      type: Object as PropType<LeaderboardEntry<string>>,
       default: () => ({}),
     },
   },
@@ -21,5 +22,10 @@ export default defineComponent({
 <style scoped>
 * {
   font-size: 1.5em;
+}
+.username {
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
 }
 </style>
